e2618f00148bb3be3d6d1e906b1493b9869d78b8
Diwaker Ghimire <idwaker@gmail.com>
2016-03-28 18:54:35 +0545

    completed if, moving into loops

--- rustbook/src/main.rs
+++ rustbook/src/main.rs
@@ -15,5 +15,15 @@ fn main() {
     // no bloody paren nice
     if x == 5 {
         println!("yes x is 5");
+    } else if x == 6 {
+        println!("me too");
+    } else {
+        println!("never see me");
     }
+    
+    // another thing with let if
+    // so if is an expression, right! 
+    let y = if x == 5 { x + 1 } else { x - 1 };
+    
+    println!("and y is: {}", y);
 }
