622b82647e7a3b5264c891e5b4852a6659557995
Diwaker Ghimire <idwaker@gmail.com>
2016-03-28 18:50:19 +0545

    if was nice but can't use /// in random place hmm.

--- rustbook/src/main.rs
+++ rustbook/src/main.rs
@@ -3,11 +3,17 @@
 //! triple quotes looks nicer than these sad
 
 
+/// these are doc comments
+/// boring kind of things to do here
+/// ok has support for markdown inside
 fn main() {
 
-    // these are comments
+    // these are comments    
     
-    /// these are doc comments
-    /// boring kind of things to do here
-    /// ok has support for markdown inside
+    let x = 5;
+    
+    // no bloody paren nice
+    if x == 5 {
+        println!("yes x is 5");
+    }
 }
