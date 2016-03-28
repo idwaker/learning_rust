80a268ba1387d650685af2a4f6f99f57ef3b265d
Diwaker Ghimire <idwaker@gmail.com>
2016-03-28 18:47:10 +0545

    comments, moving on to if

---
+++ rustbook/src/main.rs
@@ -0,0 +1,13 @@
+//! these are also doc comments
+//! but for crates, modules or functions
+//! triple quotes looks nicer than these sad
+
+
+fn main() {
+
+    // these are comments
+    
+    /// these are doc comments
+    /// boring kind of things to do here
+    /// ok has support for markdown inside
+}
