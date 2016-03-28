6005b8e68e4a2380d6f46c039bdf02fdf8200819
Diwaker Ghimire <idwaker@gmail.com>
2016-03-28 19:09:02 +0545

    completed conditionals and loops

--- rustbook/src/main.rs
+++ rustbook/src/main.rs
@@ -26,4 +26,50 @@ fn main() {
     let y = if x == 5 { x + 1 } else { x - 1 };
     
     println!("and y is: {}", y);
+    
+    // loop infinitely
+    let mut counter = 0;
+    loop {
+        counter += 1;
+        println!("Counter: {}", counter);
+        
+        if counter > 5 {
+            // break out of loop
+            break;
+        }
+    }
+    
+    // while
+    let mut done = false;
+    while !done {
+        counter += 1;
+        println!("Counter inside while: {}", counter);
+        
+        if counter > 10 {
+            done = true;
+        }
+    }
+    
+    // for works with iterator
+    // 1..10 is range ; end is exclusive
+    for i in 1..10 {
+        // then there is continue
+        if i % 2 == 0 {
+            continue;
+        }
+        println!("value of odd i: {}", i);
+    }
+    
+    for (i, j) in (5..9).enumerate() {
+        println!("Enumerate Index: {}, Value: {}", i, j);
+    }
+    
+    // labelled loops
+    'outer: for i in 1..5 {
+        'inner: for j in 1..2 {
+            if i == 2 { continue 'outer; }
+            if i == 4 { continue 'inner; }
+            println!("{}, {}", i, j);
+        }
+    }
 }
