7e6f2b5a5d295e5555c3cc28fee9e5d9809f5e6a
Diwaker Ghimire <idwaker@gmail.com>
2016-03-29 19:26:02 +0545

    enums

---
+++ rustbook/src/main.rs
@@ -0,0 +1,15 @@
+
+// enums
+enum Message {
+    Quit,
+    ChangeColor(i32, i32, i32),
+    Move { x: i32, y: i32 },
+    Write(String),
+}
+
+
+fn main() {
+    let x: Message = Message::Move { x: 12, y: 19 };
+    
+    let m = Message::Write("hello world".to_string());
+}
